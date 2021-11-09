ba293f1280de1bb871a9bf7af5da00729a0a2765
Saurabh Patel <saurabh.finch@gmail.com>
2021-11-09 07:00:46 +0530

    completed chapter 3

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,21 @@
 module.exports = {
-  "root": true,
-      "env": {
-    "node": true
-  },
-  "extends": [
+  extends: [
     "plugin:vue/vue3-essential",
-    "eslint:recommended"
+    "@vue/standard"
   ],
-      "parserOptions": {
-    "parser": "babel-eslint"
+
+  env: {
+    node: true
   },
-  "rules": {
-    "no-unused-vars": process.env.NODE_ENV === "production" ? "error" : "warn"
-  }
+
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+
+  rules: {
+    "no-unused-vars": process.env.NODE_ENV === "production" ? "error" : "warn",
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+  },
+  root: true
 }
---
+++ src/router/index.js
@@ -0,0 +1,42 @@
+import PageHome from '@/components/PageHome'
+import { createRouter, createWebHistory } from 'vue-router'
+import PageThreadShow from '@/components/PageThreadShow'
+import PageNotFound from '@/components/PageNotFound'
+import sourceData from '@/data.json'
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: PageHome
+  },
+  {
+    path: '/thread/:id',
+    name: 'ThreadShow',
+    component: PageThreadShow,
+    props: true,
+    beforeEnter (to, from, next) {
+      const threadExists = sourceData.threads.find(t => t.id === to.params.id)
+      if (threadExists) {
+        return next()
+      } else {
+        next({
+          name: 'NotFound',
+          params: { pathMatch: to.path.slice(1).split('/') },
+          query: to.query,
+          hash: to.hash
+        })
+      }
+    }
+  },
+  {
+    path: '/:pathMatch(.*)*',
+    name: 'NotFound',
+    component: PageNotFound
+  }
+]
+
+export default createRouter({
+  history: createWebHistory(),
+  routes
+})
